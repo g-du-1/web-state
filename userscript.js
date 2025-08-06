17ed868592944a5f07f0ff3a08d74076fd0e5a4b
Gergo Dudaszeg <dudaszeg1@gmail.com>
2025-08-06 23:38:35 +0100

    Add user script

---
+++ userscript.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         Scroll State Saver
+// @namespace    http://tampermonkey.net/
+// @version      2025-08-06
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.reddit.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  const savePageState = async () => {
+    const payload = {
+      url: window.location.href,
+      scrollPos: Math.trunc(window.scrollY),
+    };
+
+    GM_xmlhttpRequest({
+      method: "POST",
+      url: "http://localhost:8080/pagestate",
+      data: JSON.stringify(payload),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      onload: function (response) {
+        console.log("Success:", response.responseText);
+      },
+      onerror: function (error) {
+        console.error("Error:", error);
+      },
+    });
+  };
+
+  document.addEventListener("scrollend", savePageState);
+})();
