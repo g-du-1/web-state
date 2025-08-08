fa28aea3bbb931610ceeb11511b1614669950003
Gergo Dudaszeg <dudaszeg1@gmail.com>
2025-08-08 15:48:53 +0100

    Add text saving

--- userscript.js
+++ userscript.js
@@ -64,11 +64,52 @@
     }
   };
 
+  const getVisibleText = () => {
+    const viewport = {
+      top: window.scrollY,
+      left: window.scrollX,
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+
+    const topMargin = viewport.height * 0.1;
+    const bottomMargin = viewport.height * 0.9;
+
+    const walker = document.createTreeWalker(
+      document.body,
+      NodeFilter.SHOW_TEXT,
+      null,
+      false
+    );
+
+    let visibleText = [];
+    let node;
+
+    while ((node = walker.nextNode())) {
+      const rect = node.parentElement.getBoundingClientRect();
+
+      if (
+        rect.top >= topMargin &&
+        rect.bottom <= bottomMargin &&
+        rect.left < viewport.width &&
+        rect.right > 0
+      ) {
+        const text = node.textContent.trim();
+
+        if (text) {
+          visibleText.push(text + "\n\n");
+        }
+      }
+    }
+
+    return visibleText.join(" ");
+  };
+
   const savePageState = () => {
     const payload = {
       url: window.location.href,
       scrollPos: Math.trunc(window.scrollY),
-      visibleText: "TODO: Not implemented in js",
+      visibleText: getVisibleText(),
     };
 
     GM_xmlhttpRequest({
