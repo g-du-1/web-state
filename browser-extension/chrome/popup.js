79caad477f910af7dbc40ceca81aa53c2d3640d1
Gergo Dudaszeg <dudaszeg1@gmail.com>
2025-08-16 14:02:25 +0100

    Add types and url elem

--- browser-extension/chrome/popup.js
+++ browser-extension/chrome/popup.js
@@ -1,1 +1,20 @@
-console.log("This is a popup!")
+const apiInput = document.getElementById("api-url");
+
+if (apiInput) {
+  apiInput.addEventListener("input", (event) => {
+    const target = /** @type {HTMLInputElement} */ (event.target);
+
+    chrome.storage.local.set({ redditPageStateUrl: target.value });
+  });
+}
+
+chrome.storage.local.get("redditPageStateUrl", (result) => {
+  if (result.redditPageStateUrl) {
+    const apiInput = document.getElementById("api-url");
+
+    if (apiInput) {
+      /** @type {HTMLInputElement} */ (apiInput).value =
+        result.redditPageStateUrl;
+    }
+  }
+});
