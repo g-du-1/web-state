988103b0031361060c25fca2e1fcb097ed058a60
Gergo Dudaszeg <dudaszeg1@gmail.com>
2025-08-16 15:18:11 +0100

    Fix watch

--- browser-extension/build.js
+++ browser-extension/build.js
@@ -1,19 +1,24 @@
 import esbuild from "esbuild";
 import fs from "fs-extra";
 import path from "path";
+import { watch } from "fs";
 
 const isWatch = process.argv.includes("--watch");
 
 const srcDir = path.join(process.cwd(), "src");
 const outDir = path.join(process.cwd(), "dist");
 
-fs.copySync(
-  path.join(srcDir, "manifest.json"),
-  path.join(outDir, "manifest.json")
-);
+const copyStaticFiles = () => {
+  fs.copySync(
+    path.join(srcDir, "manifest.json"),
+    path.join(outDir, "manifest.json")
+  );
 
-fs.copySync(path.join(srcDir, "popup.html"), path.join(outDir, "popup.html"));
-fs.copySync(path.join(srcDir, "popup.css"), path.join(outDir, "popup.css"));
+  fs.copySync(path.join(srcDir, "popup.html"), path.join(outDir, "popup.html"));
+  fs.copySync(path.join(srcDir, "popup.css"), path.join(outDir, "popup.css"));
+};
+
+copyStaticFiles();
 
 (async () => {
   const buildOptions = {
@@ -32,6 +37,15 @@ fs.copySync(path.join(srcDir, "popup.css"), path.join(outDir, "popup.css"));
     const context = await esbuild.context(buildOptions);
     await context.watch();
     console.log("Watching for changes...");
+
+    watch(srcDir, (_eventType, filename) => {
+      if (
+        filename &&
+        ["manifest.json", "popup.html", "popup.css"].includes(filename)
+      ) {
+        copyStaticFiles();
+      }
+    });
   } else {
     await esbuild.build(buildOptions);
     console.log("Build finished!");
