ba4e0946ffc46e8161198e7d00f2490761a88084
Gergo Dudaszeg <dudaszeg1@gmail.com>
2025-08-16 16:03:07 +0100

    Add whitelist elem

--- browser-extension/src/popup.ts
+++ browser-extension/src/popup.ts
@@ -1,4 +1,5 @@
 const apiInput = document.getElementById("api-url");
+const whitelistInput = document.getElementById("whitelist-sites");
 
 if (apiInput) {
   apiInput.addEventListener("input", (event) => {
@@ -8,12 +9,28 @@ if (apiInput) {
   });
 }
 
-chrome.storage.local.get("pageStateApiUrl", (result) => {
+if (whitelistInput) {
+  whitelistInput.addEventListener("input", (event) => {
+    const target = event.target as HTMLInputElement;
+
+    chrome.storage.local.set({ whitelistSites: target.value });
+  });
+}
+
+chrome.storage.local.get(["pageStateApiUrl", "whitelistSites"], (result) => {
   if (result.pageStateApiUrl) {
     const apiInput = document.getElementById("api-url");
 
     if (apiInput) {
       (apiInput as HTMLInputElement).value = result.pageStateApiUrl;
+    }
+  }
+
+  if (result.whitelistSites) {
+    const whitelistInput = document.getElementById("whitelist-sites");
+
+    if (whitelistInput) {
+      (whitelistInput as HTMLInputElement).value = result.whitelistSites;
     }
   }
 });
